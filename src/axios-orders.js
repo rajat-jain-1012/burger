30c18b5c7548c025a52ec77d5c7539ff6b4a73fa
unknown <jain.rajat1012@gmail.com>
2020-07-02 11:50:39 +0530

    firebase

---
+++ src/axios-orders.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL : 'https://burger-builder-307fe.firebaseio.com/',
+
+});
+
+export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,8 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad :20,
@@ -98,7 +99,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('Burger Ordered Succesfully');
+        // alert('Burger Ordered Succesfully');
+        const order = {
+            ingredients : this.state.ingredients,
+            price : this.state.price,
+            customer : {
+                name : 'Rajat',
+                address : {
+                    street : 'Kala pathar road',
+                    code : '201210',
+                    country : 'india'
+                },
+                email : 'test@testmail'                
+            },
+            delievryMethod : 'fastest',
+        };
+        axios.post('/orders.json', order)
+              .then( response =>
+                console.log(response)
+              ).catch(error => console.log(error));
+
     }
 
     render() {
